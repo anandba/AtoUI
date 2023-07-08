b95a53dea8df02a762996c39fd57226ec2581085
EraiDev <anand@eraitechnologies.com>
2023-07-08 12:33:53 +0530

    website fixes

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,46 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+require '/home/anandjap2210/PHPMailer-master/src/Exception.php';
+require '/home/anandjap2210/PHPMailer-master/src/PHPMailer.php';
+require '/home/anandjap2210/PHPMailer-master/src/SMTP.php';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Instantiation and passing [ICODE]true[/ICODE] enables exceptions
+$mail = new PHPMailer(true);
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+try {
+ //Server settings
+ $mail->SMTPDebug = 2; // Enable verbose debug output
+ $mail->isSMTP(); // Set mailer to use SMTP
+ $mail->Host = 'smtp.office365.com'; // Specify main and backup SMTP servers
+ $mail->SMTPAuth = true; // Enable SMTP authentication
+ $mail->Username = 'admin@eraitechnologies.com'; // SMTP username
+ $mail->Password = 'iotproduct*22'; // SMTP password
+ $mail->SMTPSecure = 'tls'; // Enable TLS encryption, [ICODE]ssl[/ICODE] also accepted
+ $mail->Port = 587; // TCP port to connect to
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+//Recipients
+ $mail->setFrom('admin@eraitechnologies.com', 'Mailer');
+ $mail->addAddress('anand@eraitechnologies.com', 'Anand Erai'); // Add a recipient
+ $mail->addAddress('anand@eraitechnologies.com'); // Name is optional
+ $mail->addReplyTo('admin@eraitechnologies.com', 'Admin');
+ //$mail->addCC('cc@example.com');
+ //$mail->addBCC('bcc@example.com');
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// Attachments
+ //$mail->addAttachment('/home/anandjap2210/attachment.txt'); // Add attachments
+ //$mail->addAttachment('/home/anandjap2210/image.jpg', 'new.jpg'); // Optional name
 
-  echo $contact->send();
-?>
+// Content
+ $mail->isHTML(true); // Set email format to HTML
+ $mail->Subject = 'Website Test Mail';
+ $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+ $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+$mail->send();
+ echo 'Message has been sent';
+
+} catch (Exception $e) {
+ echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
